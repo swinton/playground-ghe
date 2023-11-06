90bb056217118d254ed562ba9d1c688520c2498d
Steve Winton <stevewinton@gmail.com>
2023-11-06 16:59:46 -0600

    Add repos.tf

--- repos.tf
+++ repos.tf
@@ -1,5 +1,16 @@
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome repo is awesome"
-  visibility  = "internal"
+resource "github_repository" "repo" {
+  for_each = {
+    for repo in csvdecode(file("repos.csv")) :
+    repo.name => repo
+  }
+
+  name = each.value.name
+  description = "my awesome repo is awesome"
+  visibility = "internal"
+  has_issues = false
+  has_projects = false
+  has_wiki = false
+  auto_init = true
+  gitignore_template = "Node"
+  license_template = "mit"
 }
